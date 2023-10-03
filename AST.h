528da261345c8bfa0aa8f894dd1e7c292c2ea598
JixS4v <61665800+JixS4v@users.noreply.github.com>
2023-10-03 09:24:49 +0200

    the abstract syntax tree is real (can we get much higher)

---
+++ AST.h
@@ -0,0 +1,38 @@
+#include "types.h"
+
+typedef struct NODE NODE; //Forward reference
+typedef struct TREE TREE;
+typedef struct FLOW FLOW;
+
+// A basic node, one operation that can be nested if one value references the result of another operation
+struct NODE{
+	enum {
+		NUMBER,
+		DEFINT,
+		ASSIGN,
+		ADD,
+	}tag;
+	union {
+		struct NUMBER {int number;} NUMBER;// regular-ass number 
+		struct IDENTIFIER {char* identifier;} IDENTIFIER;// variable name
+		struct DEFINE { NODE *identifier; datatype type;} DEFINT;// define operator of a variable
+		struct ASSIGN { NODE *identifier; NODE *value;} ASSIGN;// opperation to assign a variable
+	       	struct ADD { NODE *left; NODE *right} ADD;// performs a sum of two values	
+	}data;
+}
+//Linked List elementst that contain different operations(define and assign for instance), linear execution.
+struct TREE{
+	NODE node;
+	TREE* next
+}
+
+struct FUNCTION{
+	//arguments, malloced array
+	NODE* arguments;
+	//pointer to the first node of the linked list
+	TREE* first;
+	//returning functionality
+	type returnType;
+	NODE* returnValue;
+}	
+
