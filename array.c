a010fc48e57e9d1ea00a10ca77527ae39c0789b8
jixs4v <fakeemail@email.com>
2023-10-05 16:19:00 +0200

    I FIXED A SEGFAULT RAHHHHH

--- array.c
+++ array.c
@@ -4,6 +4,6 @@
 
 void* append_to_array(void* array, int array_current_index, void* to_append, size_t size_of_type){
 	void* arrayPtr = realloc(array , size_of_type*(array_current_index+ 1));
-	memcpy(arrayPtr+array_current_index, to_append, size_of_type);
+	memcpy(arrayPtr+(array_current_index*size_of_type), to_append, size_of_type);
 	return arrayPtr;
 }
